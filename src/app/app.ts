b74c00707a819ba16071931a82f4a0423f400fb5
Christian <cwiemann39@gmial.com>
2026-07-28 15:21:55 +0200

    feat: add about me section with image, box and highlights

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, About],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
